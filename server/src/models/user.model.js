e4d9051827158334b911537502acc076609a9cdb
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-18 20:35:04 +0530

    feat: implement user model with schema validation

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,6 +1,52 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({}, { timestamps: true });
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 100,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+            select: false,
+        },
+
+        avatar: {
+            type: String,
+            default: "",
+        },
+
+        refreshToken: {
+            type: String,
+            default: null,
+            select: false,
+        },
+
+        emailVerified: {
+            type: Boolean,
+            default: false,
+        },
+
+        lastLoginAt: {
+            type: Date,
+            default: null,
+        },
+    },
+    { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
