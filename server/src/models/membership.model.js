f82b3266abffc73bd69685494a84c7969653d450
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-18 20:45:40 +0530

    add membership model

--- server/src/models/membership.model.js
+++ server/src/models/membership.model.js
@@ -1,6 +1,39 @@
 import mongoose, { Schema } from "mongoose";
 
-const membershipSchema = new Schema({}, { timestamps: true });
+const membershipSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        organization: {
+            type: Schema.Types.ObjectId,
+            ref: "Organization",
+            required: true,
+        },
+
+        role: {
+            type: String,
+            enum: ["owner", "admin", "member"],
+            default: "member",
+            required: true,
+        },
+
+        status: {
+            type: String,
+            enum: ["active", "invited", "suspended"],
+            default: "active",
+        },
+
+        joinedAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    { timestamps: true }
+);
 
 const Membership = mongoose.model("Membership", membershipSchema);
 
