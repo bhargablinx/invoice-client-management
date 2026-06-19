a2832b335ad1db10292cf29932ee0571cd60849e
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 19:40:17 +0530

    Remove unused routes

--- server/src/routes/resendMail.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { resendMail } from "../controllers/resendMail.controller.js";
-
-const router = Router();
-
-router.route("/").post(resendMail);
-
-export default router;
--- server/src/routes/verifyMail.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { verifyMail } from "../controllers/verifyMail.controller.js";
-
-const router = Router();
-
-router.route("/:token").get(verifyMail);
-
-export default router;
