bb0036e2533d096ba68e0f676c7e9d388bd90e64
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 13:49:14 +0530

    Instal bcrypt and setup encrypt & decrypt password

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -48,6 +48,17 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
+// Encrypt password
+userSchema.pre("save", async function () {
+    if (this.isModified("password"))
+        this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Decrypt password and validate
+userSchema.methods.isPasswordCorrect = async function (passwordByClient) {
+    return await bcrypt.compare(passwordByClient, this.password); // returns bool
+};
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
