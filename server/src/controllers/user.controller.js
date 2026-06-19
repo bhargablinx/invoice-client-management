67d12d6e3dc3fe8aec09586d494fb4a7a4ebbbd1
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 13:46:43 +0530

    add signup controller (incomplete)

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,28 @@
+import ApiResponse from "../utils/ApiResponse.js";
+import ApiError from "../utils/ApiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import User from "../models/user.model.js";
+
+const signup = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password)
+        throw new ApiError(400, "Required Filed is Missing!");
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser)
+        throw new ApiError(400, "User already exists with this email!");
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    res.status(200).json(
+        new ApiResponse(200, user, "User registered successfully!")
+    );
+});
+
+export { signup };
