713bc7ae8decd1b858fa15e055d3dbbb3c6e6bae
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 15:22:17 +0530

    Add logout endpoint in user router

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { signup, login } from "../controllers/user.controller.js";
+import { signup, login, logout } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/signup").post(upload.single("avatar"), signup);
 router.route("/login").post(login);
+router.route("/logout").post(verifyJWT, logout);
 
 export default router;
