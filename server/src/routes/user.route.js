0bd57dd252b59b0c7b39ac44b4b7f891125da934
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 14:00:49 +0530

    Setup multer

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { signup } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/signup").post(signup);
+router.route("/signup").post(upload.single("avatar"), signup);
 
 export default router;
