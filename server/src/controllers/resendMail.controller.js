4931b8d8a80be4c4cade6d23463f1c412c0b59db
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 17:53:50 +0530

    Remove old logic with new generate email token

--- server/src/controllers/resendMail.controller.js
+++ server/src/controllers/resendMail.controller.js
@@ -11,18 +11,15 @@ const resendMail = asyncHandler(async (req, res) => {
 
     if (!user) throw new ApiError(400, null, "Invalid Email");
 
-    const unHashedEmailToken = crypto.randomBytes(32).toString("hex");
-    const hashedEmailToken = crypto
-        .createHash("sha256")
-        .update(unHashedEmailToken)
-        .digest("hex");
-    user.emailVerificationToken = hashedEmailToken;
-    user.emailVerificationTokenExpiry = Date.now() + 1000 * 60 * 60; // 1 hour
-
+    const newVerificationToken = user.generateEmailToken();
     await user.save({ validateBeforeSave: false });
 
     res.status(200).json(
-        new ApiResponse(200, unHashedEmailToken, "New Verification Mail Sent!!")
+        new ApiResponse(
+            200,
+            newVerificationToken,
+            "New Verification Mail Sent!!"
+        )
     );
 });
 
