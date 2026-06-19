9b09365812e9e19e1a9fb8f15bcc4eb2d3e0ff2f
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 14:08:54 +0530

    add signup controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -2,9 +2,11 @@ import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import User from "../models/user.model.js";
+import { uploadToCloudinary } from "../utils/uploadToCloudinary.js";
 
 const signup = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
+    const avatarPath = req.file.path;
 
     if (!name || !email || !password)
         throw new ApiError(400, "Required Filed is Missing!");
@@ -14,10 +16,14 @@ const signup = asyncHandler(async (req, res) => {
     if (existingUser)
         throw new ApiError(400, "User already exists with this email!");
 
+    const cloudinaryResponse = await uploadToCloudinary(avatarPath);
+    const avatar = cloudinaryResponse.url;
+
     const user = await User.create({
         name,
         email,
         password,
+        avatar,
     });
 
     res.status(200).json(
