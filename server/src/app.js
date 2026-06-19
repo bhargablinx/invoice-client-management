9a64223c2e46fc03be0faeb3ca4946b5e2e537c9
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 19:40:01 +0530

    Change verify and resend email route

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import healthRouter from "./routes/healthCheck.route.js";
-import userRouter from "./routes/auth.route.js";
+import authRouter from "./routes/auth.route.js";
 import errorHandler from "./middlewares/errorHandler.middleware.js";
 import verifyMailRouter from "./routes/verifyMail.route.js";
 import resendMailRouter from "./routes/resendMail.route.js";
@@ -20,9 +20,7 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.use("/api/v1/healthcheck", healthRouter);
-app.use("/api/v1/auth", userRouter);
-app.use("/api/v1/verify-email", verifyMailRouter);
-app.use("/api/v1/resend-email", resendMailRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use(errorHandler);
 
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -4,6 +4,8 @@ import {
     login,
     logout,
     changePassword,
+    verifyMail,
+    resendMail,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -14,5 +16,7 @@ router.route("/signup").post(upload.single("avatar"), signup);
 router.route("/login").post(login);
 router.route("/logout").post(verifyJWT, logout);
 router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/verify-email/:token").get(verifyMail);
+router.route("/resend-email").post(resendMail);
 
 export default router;
