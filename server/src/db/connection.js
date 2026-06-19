e49add9bd267ab8b31257b8d2c3b2c03bade355d
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 13:34:27 +0530

    Update MongoDB url

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/x-db`);
+        await mongoose.connect(`${process.env.MONGODB_URL}/invoice-client-db`);
         console.log("DB Connected!");
     } catch (error) {
         console.log("FAILED CONNECTION!!", error);
