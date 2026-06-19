4440f12bdf225d04cb4d1474e7d9a219fded3dd8
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 19:39:24 +0530

    Migrate resend mail and verify mail controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import User from "../models/user.model.js";
 import { uploadToCloudinary } from "../utils/uploadToCloudinary.js";
+import crypto from "crypto";
 
 const cookieOption = {
     httpOnly: true,
@@ -134,4 +135,50 @@ const changePassword = asyncHandler(async (req, res) => {
     );
 });
 
-export { signup, login, logout, changePassword };
+const resendMail = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) throw new ApiError(400, null, "Invalid Email");
+
+    const newVerificationToken = user.generateEmailToken();
+    await user.save({ validateBeforeSave: false });
+
+    res.status(200).json(
+        new ApiResponse(
+            200,
+            newVerificationToken,
+            "New Verification Mail Sent!!"
+        )
+    );
+});
+
+const verifyMail = asyncHandler(async (req, res) => {
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(req.params.token)
+        .digest("hex");
+
+    const user = await User.findOne({
+        emailVerificationToken: hashedToken,
+        emailVerificationTokenExpiry: {
+            $gt: Date.now(),
+        },
+    });
+
+    if (!user) {
+        throw new ApiError(400, "Invalid or expired verification token");
+    }
+
+    user.isEmailVerified = true;
+
+    user.emailVerificationToken = undefined;
+    user.emailVerificationTokenExpiry = undefined;
+
+    await user.save();
+
+    res.status(200).json(new ApiResponse(200, user, "Email Verified!!"));
+});
+
+export { signup, login, logout, changePassword, resendMail, verifyMail };
