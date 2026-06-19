958505190114c1fd59cb366083e658d94fa2d9d0
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-19 12:39:18 +0530

    Add middlewares cors, json limit, cookie-parser

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+    })
+);
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 export default app;
