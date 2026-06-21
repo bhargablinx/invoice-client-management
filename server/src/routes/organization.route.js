3a52a603005017f61c2aef8fb517bba682a3444a
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-21 13:49:21 +0530

    bug: get -> delete

--- server/src/routes/organization.route.js
+++ server/src/routes/organization.route.js
@@ -24,6 +24,8 @@ router.route("/:organizationId/members").get(verifyJWT, getMembers);
 router
     .route("/:organizationId/members/:userId")
     .patch(verifyJWT, changeMemberRole);
-router.route("/:organizationId/members/:userId").get(verifyJWT, removeMember);
+router
+    .route("/:organizationId/members/:userId")
+    .delete(verifyJWT, removeMember);
 
 export default router;
