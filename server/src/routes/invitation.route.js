f63042546965b561a6726c4dcc49d38b14c7ee76
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-21 21:35:35 +0530

    bug: get -> post

--- server/src/routes/invitation.route.js
+++ server/src/routes/invitation.route.js
@@ -7,7 +7,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/:token/accept").get(verifyJWT, acceptInvitation);
+router.route("/:token/accept").post(verifyJWT, acceptInvitation);
 router.route("/:token/reject").post(verifyJWT, rejectInvitation);
 
 export default router;
