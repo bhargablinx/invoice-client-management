f19d0c236ce4044ed670382db32aa139125d354b
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-21 17:51:42 +0530

    Add role based access

--- server/src/routes/organization.route.js
+++ server/src/routes/organization.route.js
@@ -19,20 +19,45 @@ import {
 
 const router = Router();
 
+// Any authorized user can create
 router.route("/").post(upload.single("logo"), verifyJWT, createOrganization);
-router.route("/:organizationId").get(verifyJWT, getOrganization);
-router.route("/:organizationId").patch(verifyJWT, updateOrganization);
-router.route("/:organizationId").delete(verifyJWT, deleteOrganization);
 
-router.route("/:organizationId/members").get(verifyJWT, getMembers);
+// User for that organization can get info
+router
+    .route("/:organizationId")
+    .get(
+        verifyJWT,
+        authorizeRoles("owner", "admin", "member"),
+        getOrganization
+    );
+
+// Only owner can update organization info and delete
+router
+    .route("/:organizationId")
+    .patch(verifyJWT, authorizeRoles("owner"), updateOrganization);
+router
+    .route("/:organizationId")
+    .delete(verifyJWT, authorizeRoles("owner"), deleteOrganization);
+
+// Any org member can fetch members
+router
+    .route("/:organizationId/members")
+    .get(verifyJWT, authorizeRoles("owner", "admin", "member"), getMembers);
+
+// Only owner can
 router
     .route("/:organizationId/members/:userId")
-    .patch(verifyJWT, changeMemberRole);
+    .patch(verifyJWT, authorizeRoles("owner"), changeMemberRole);
 router
     .route("/:organizationId/members/:userId")
-    .delete(verifyJWT, removeMember);
+    .delete(verifyJWT, authorizeRoles("owner"), removeMember);
 
-router.route("/:organizationId/invitations").post(verifyJWT, inviteUser);
-router.route("/:organizationId/invitations").get(verifyJWT, getInvitations);
+// Owner & Admin can invite members
+router
+    .route("/:organizationId/invitations")
+    .post(verifyJWT, authorizeRoles("owner", "admin"), inviteUser);
+router
+    .route("/:organizationId/invitations")
+    .get(verifyJWT, authorizeRoles("owner", "admin"), getInvitations);
 
 export default router;
