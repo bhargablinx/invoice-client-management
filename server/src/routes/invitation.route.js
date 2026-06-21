06d99586c08feeb63d6ca32bc3c82af95595ac13
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-21 16:04:16 +0530

    Add route for reject invitation

--- server/src/routes/invitation.route.js
+++ server/src/routes/invitation.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { acceptInvitation } from "../controllers/invitation.controller.js";
+import {
+    acceptInvitation,
+    rejectInvitation,
+} from "../controllers/invitation.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/:token/accept").get(verifyJWT, acceptInvitation);
+router.route("/:token/reject").get(verifyJWT, rejectInvitation);
 
 export default router;
