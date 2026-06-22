5b2b0b9d64b7db631ca34c0fce92945cb6a34e3a
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-22 13:49:49 +0530

    Add timestamps in schema

--- server/src/models/invoice.model.js
+++ server/src/models/invoice.model.js
@@ -1,96 +1,101 @@
 import mongoose, { Schema } from "mongoose";
 
-const invoiceSchema = new Schema({
-    organization: {
-        type: Schema.Types.ObjectId,
-        ref: "Organization",
-        required: true,
-        index: true,
-    },
+const invoiceSchema = new Schema(
+    {
+        organization: {
+            type: Schema.Types.ObjectId,
+            ref: "Organization",
+            required: true,
+            index: true,
+        },
 
-    client: {
-        type: Schema.Types.ObjectId,
-        ref: "Client",
-        required: true,
-    },
+        client: {
+            type: Schema.Types.ObjectId,
+            ref: "Client",
+            required: true,
+        },
 
-    invoiceNumber: {
-        type: String,
-        required: true,
-    },
+        invoiceNumber: {
+            type: String,
+            required: true,
+        },
 
-    issueDate: {
-        type: Date,
-        required: true,
-        default: Date.now,
-    },
+        issueDate: {
+            type: Date,
+            required: true,
+            default: Date.now,
+        },
 
-    dueDate: {
-        type: Date,
-        required: true,
-    },
+        dueDate: {
+            type: Date,
+            required: true,
+        },
 
-    status: {
-        type: String,
-        enum: [
-            "draft",
-            "sent",
-            "viewed",
-            "partially_paid",
-            "paid",
-            "overdue",
-            "cancelled",
-        ],
-        default: "draft",
-    },
+        status: {
+            type: String,
+            enum: [
+                "draft",
+                "sent",
+                "viewed",
+                "partially_paid",
+                "paid",
+                "overdue",
+                "cancelled",
+            ],
+            default: "draft",
+        },
 
-    currency: {
-        type: String,
-        default: "INR",
-    },
+        currency: {
+            type: String,
+            default: "INR",
+        },
 
-    subtotal: {
-        type: Number,
-        required: true,
-        min: 0,
-    },
+        subtotal: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
 
-    taxAmount: {
-        type: Number,
-        default: 0,
-        min: 0,
-    },
+        taxAmount: {
+            type: Number,
+            default: 0,
+            min: 0,
+        },
 
-    discountAmount: {
-        type: Number,
-        default: 0,
-        min: 0,
-    },
+        discountAmount: {
+            type: Number,
+            default: 0,
+            min: 0,
+        },
 
-    totalAmount: {
-        type: Number,
-        required: true,
-        min: 0,
-    },
+        totalAmount: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
 
-    amountPaid: {
-        type: Number,
-        default: 0,
-        min: 0,
-    },
+        amountPaid: {
+            type: Number,
+            default: 0,
+            min: 0,
+        },
 
-    balanceDue: {
-        type: Number,
-        required: true,
-        min: 0,
-    },
+        balanceDue: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
 
-    createdBy: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
     },
-});
+    {
+        timestamps: true,
+    }
+);
 
 invoiceSchema.index(
     {
