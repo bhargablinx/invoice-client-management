e402ea0f44e8ee9a97642289e8c666b78f3d6abb
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-22 13:20:03 +0530

    Add timestamps in schema

--- server/src/models/invoiceItem.model.js
+++ server/src/models/invoiceItem.model.js
@@ -1,50 +1,53 @@
 import mongoose, { Schema } from "mongoose";
 
-const invoiceItemSchema = new Schema({
-    invoice: {
-        type: Schema.Types.ObjectId,
-        ref: "Invoice",
-        required: true,
-        index: true,
-    },
+const invoiceItemSchema = new Schema(
+    {
+        invoice: {
+            type: Schema.Types.ObjectId,
+            ref: "Invoice",
+            required: true,
+            index: true,
+        },
 
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 500,
-    },
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 500,
+        },
 
-    quantity: {
-        type: Number,
-        required: true,
-        min: 1,
-    },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,
+        },
 
-    unitPrice: {
-        type: Number,
-        required: true,
-        min: 0,
-    },
+        unitPrice: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
 
-    taxRate: {
-        type: Number,
-        default: 0,
-        min: 0,
-    },
+        taxRate: {
+            type: Number,
+            default: 0,
+            min: 0,
+        },
 
-    discountAmount: {
-        type: Number,
-        default: 0,
-        min: 0,
-    },
+        discountAmount: {
+            type: Number,
+            default: 0,
+            min: 0,
+        },
 
-    lineTotal: {
-        type: Number,
-        required: true,
-        min: 0,
+        lineTotal: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
     },
-});
+    { timestamps: true }
+);
 
 const Invoiceitem = mongoose.model("Invoiceitem", invoiceItemSchema);
 
