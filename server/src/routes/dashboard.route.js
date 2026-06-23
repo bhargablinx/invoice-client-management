554e89494fa9ccf66ee9a6ba7725d65f921a7379
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-23 13:54:33 +0530

    Add dashboard controller endpoints

--- server/src/routes/dashboard.route.js
+++ server/src/routes/dashboard.route.js
@@ -1,7 +1,21 @@
 import { Router } from "express";
+import {
+    getMonthlyRevenue,
+    getOverview,
+    getRecentInvoices,
+    getTopClients,
+} from "../controllers/dashboard.controller.js";
+import { authorizeRoles, verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/").get();
+router.route("/:organizationId/overview").get(verifyJWT, getOverview);
+router
+    .route("/:organizationId/monthly-revenue")
+    .get(verifyJWT, getMonthlyRevenue);
+router
+    .route("/:organizationId/recent-invoices")
+    .get(verifyJWT, getRecentInvoices);
+router.route("/:organizationId/top-clients").get(verifyJWT, getTopClients);
 
 export default router;
