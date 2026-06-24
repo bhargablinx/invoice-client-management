983373421ba7beeee17d17fa41f3448a54094e34
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-24 14:33:03 +0530

    Update var

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
 
 function ProtectedRoute() {
-    const { isLogin } = useSelector((state) => state.auth);
+    const { isAuthenticated } = useSelector((state) => state.auth);
 
-    if (!isLogin) return <Navigate to="/login" replace />;
+    if (!isAuthenticated) return <Navigate to="/login" replace />;
 
     return <Outlet />;
 }
