0f6b1c91ebbf172916c22773fc857a9ba0d59ac8
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-24 13:55:05 +0530

    Setup redux store

---
+++ client/src/features/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isLogin: false,
+    userData: null,
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            ((state.isLogin = true), (state.userData = action.payload));
+        },
+        logout: (state, action) => {
+            ((state.isLogin = false), (state.userData = null));
+        },
+    },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
