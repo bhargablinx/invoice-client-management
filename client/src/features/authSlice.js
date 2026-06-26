b21aec2de09549ba4e6209274fd4ec9fedce1341
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-26 13:26:50 +0530

    Update auth slice

--- client/src/features/authSlice.js
+++ client/src/features/authSlice.js
@@ -2,27 +2,55 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     user: null,
-    isAuthorized: true,
+    isAuthenticated: false,
+    accessToken: null,
+    loading: false,
 };
 
 export const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
+        setLoading: (state, action) => {
+            state.loading = action.payload;
+        },
+
         login: (state, action) => {
             state.user = action.payload;
             state.isAuthorized = true;
         },
+
         signup: (state, action) => {
             state.user = action.payload;
             state.isAuthorized = true;
         },
-        logout: (state, action) => {
+
+        logout: (state) => {
             state.user = null;
-            state.isAuthorized = false;
+            state.accessToken = null;
+            state.isAuthenticated = false;
+            state.loading = false;
+        },
+
+        updateUser: (state, action) => {
+            state.user = {
+                ...state.user,
+                ...action.payload,
+            };
+        },
+
+        updateAccessToken: (state, action) => {
+            state.accessToken = action.payload;
         },
     },
 });
 
-export const { login, signup, logout } = authSlice.actions;
+export const {
+    login,
+    signup,
+    logout,
+    setLoading,
+    updateAccessToken,
+    updateUser,
+} = authSlice.actions;
 export default authSlice.reducer;
