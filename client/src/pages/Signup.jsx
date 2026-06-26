1a369bedac85c235342d04e9413a4568100bdccb
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-26 17:12:54 +0530

    Remove unused imports

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Loading from "@/components/Loading";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
