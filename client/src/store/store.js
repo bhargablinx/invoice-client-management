f840c3fb59f3ae6f12114ef2798f96f3927e0001
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-27 15:56:17 +0530

    Update store by adding new reducers

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/features/auth/authSlice";
+import clientReducer from "@/features/clients/clientSlice";
+import invoiceReducer from "@/features/invoices/invoiceSlice";
+import paymentReducer from "@/features/payments/paymentSlice";
+import serviceReducer from "@/features/services/serviceSlice";
+import organizationReducer from "@/features/organization/organizationSlice";
+import dashboardReducer from "@/features/dashboard/dashboardSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        clients: clientReducer,
+        invoices: invoiceReducer,
+        payments: paymentReducer,
+        services: serviceReducer,
+        organization: organizationReducer,
+        dashboard: dashboardReducer,
     },
 });
