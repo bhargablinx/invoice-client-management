bf7af826bbf6dc4d47b5df2ccef1168c03aa4cc1
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-28 16:51:23 +0530

    Make Invoice stats dynamic

--- client/src/components/manage-invoices/InvoiceStats.jsx
+++ client/src/components/manage-invoices/InvoiceStats.jsx
@@ -1,31 +1,70 @@
 import { Clock3, FileText, Pencil, CircleDollarSign } from "lucide-react";
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
-const stats = [
-    {
-        title: "Total Invoices",
-        value: "284",
-        icon: FileText,
-    },
-    {
-        title: "Drafts",
-        value: "12",
-        icon: Pencil,
-    },
-    {
-        title: "Pending",
-        value: "54",
-        icon: Clock3,
-    },
-    {
-        title: "Paid",
-        value: "218",
-        icon: CircleDollarSign,
-    },
-];
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const InvoiceStats = () => {
+    const { invoices } = useSelector((state) => state.invoices);
+    const [stats, setStats] = useState([
+        {
+            title: "Total Invoices",
+            value: 0,
+            icon: FileText,
+        },
+        {
+            title: "Drafts",
+            value: 0,
+            icon: Pencil,
+        },
+        {
+            title: "Pending",
+            value: 0,
+            icon: Clock3,
+        },
+        {
+            title: "Paid",
+            value: 0,
+            icon: CircleDollarSign,
+        },
+    ]);
+    useEffect(() => {
+        const allDraftInvoices = invoices.filter(
+            (invoice) => invoice.status === "draft",
+        );
+        const allPendingInvoices = invoices.filter(
+            (invoice) =>
+                invoice.status === "sent" ||
+                invoice.status === "viewed" ||
+                invoice.status === "partially_paid",
+        );
+        const allPaidInvoices = invoices.filter(
+            (invoice) => invoice.status === "paid",
+        );
+
+        setStats([
+            {
+                title: "Total Invoices",
+                value: invoices.length,
+                icon: FileText,
+            },
+            {
+                title: "Drafts",
+                value: allDraftInvoices.length,
+                icon: Pencil,
+            },
+            {
+                title: "Pending",
+                value: allPendingInvoices.length,
+                icon: Clock3,
+            },
+            {
+                title: "Paid",
+                value: allPaidInvoices.length,
+                icon: CircleDollarSign,
+            },
+        ]);
+    }, [invoices]);
+
     return (
         <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-4">
             {stats.map((stat) => {
